549330636c7275d35b3c07b0a21d51aa3dfc6666
novaventurelane <linklemodding@gmail.com>
2022-04-13 19:19:33 -0700

    blocks

--- src/main/java/net/linkle/newgrowth/z_INIT/NG_Blocks.java
+++ src/main/java/net/linkle/newgrowth/z_INIT/NG_Blocks.java
@@ -17,6 +17,8 @@ import static net.linkle.newgrowth.z_INIT.NG_Groups.TEST_GROUP;
 public class NG_Blocks {
     public static final Block SALT_ORE = new OreBlock(Block.Settings.copy(Blocks.COAL_ORE), UniformIntProvider.create(0, 2));
     public static final Block SALT_BLOCK = new StoneBlock();
+    public static final Block CHARCOAL_BLOCK = new StoneBlock();
+
     public static final Block NETHER_SALT = new OreBlock(Block.Settings.copy(Blocks.NETHER_QUARTZ_ORE), UniformIntProvider.create(0, 2));
     public static final Block NETHER_COAL_ORE = new OreBlock(Block.Settings.copy(Blocks.NETHER_QUARTZ_ORE), UniformIntProvider.create(0, 2));
 
@@ -29,7 +31,9 @@ public class NG_Blocks {
 
         Reg.registerWithItem("salt_ore", SALT_ORE, testGroup);
         Reg.registerWithItem("salt_block", SALT_BLOCK, testGroup);
-        Reg.registerWithItem("nether_salt", NETHER_SALT, testGroup);
+        Reg.registerWithItem("charcoal_block", CHARCOAL_BLOCK, testGroup);
+
+        Reg.registerWithItem("nether_salt_ore", NETHER_SALT, testGroup);
         Reg.registerWithItem("nether_coal_ore", NETHER_COAL_ORE, testGroup);
 
     }
